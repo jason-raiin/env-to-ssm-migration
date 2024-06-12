f29dbb304a4b2c8e6711defd9bf50340b7454498
Jason <jiacan.h01@gmail.com>
2024-06-12 12:39:21 -0400

    Add file to convert AWS SSM into JSON for task definition

---
+++ import-to-task-def.js
@@ -0,0 +1,37 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+
+// config (You only need to edit this part)
+const SSM = new AWS.SSM({
+  region: 'ca-central-1', // Specify AWS region
+  accessKeyId: "",
+  secretAccessKey: "",
+  sessionToken: ""
+});
+
+async function getParams(nextToken, paramList) {
+  SSM.getParametersByPath({
+    Path: '/project/env/',
+    Recursive: true,
+    WithDecryption: true,
+    MaxResults: 10,
+    NextToken: nextToken
+  }).send((_, data) => {
+    paramList.push(...data.Parameters || []);
+    if (data.Parameters?.length === 10) getParams(data.NextToken, paramList)
+    else {
+      const paramDefinitions = paramList.map(param => {
+        const name = param.Name.replace('/project/env/', '');
+        const arn = param.ARN;
+        return { "name": name, "arn": arn };
+      });
+      fs.writeFile('./params.json', JSON.stringify(paramDefinitions), function (err) {
+        if (err) return console.log(err);
+        console.log("The file was saved!");
+      });
+    }
+  });
+}
+
+const paramList = [];
+getParams(undefined, paramList)
